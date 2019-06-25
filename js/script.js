e833af43b5c64ed56bf7d6da681a64dd4d899d29
Artur Kvasnikov <artcshq@gmail.com>
2019-06-25 20:35:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let btn = document.getElementById("start"),
+    budget = document.getElementsByClassName("budget-value"),
+    daybudget = document.getElementsByClassName("daybudget-value"),
+    level = document.getElementsByClassName("level-value"),
+    expenses = document.getElementsByClassName("expenses-value"),
+    optionalexpenses = document.getElementsByClassName("optionalexpenses-value"),
+    income = document.getElementsByClassName("income-value"),
+    monthsavings = document.getElementsByClassName("monthsavings-value"),
+    yearsavings = document.getElementsByClassName("yearsavings-value");
+
+let input = document.querySelectorAll(".expenses-item");
+console.log(input);
+
+let buttons = document.getElementsByTagName("button"),
+    confirmbtn1 = buttons[0],
+    confirmbtn2 = buttons[1],
+    count = buttons[2];
+
+    
+let optionalExpensesItem = document.querySelectorAll(".optionalexpenses-item");
+
+let percent = document.querySelector("#percent"),
+    year = document.querySelector(".year");
+
+
+let test = document.querySelector(".title");
+
+
+test.style.fontSize = "30px";
+
+console.log(test);
+
